d303933d5cf0227cdb6ab64d5ff71c89bb4fc8cd
Nick K <nick@arguflow.gg>
2023-09-11 20:49:04 -0500

    content: brands+partnerships

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -8,7 +8,7 @@ import { SpotifyLogo } from "../assets/logos/SpotifyLogo";
 import { StripeLogo } from "../assets/logos/StripeLogo";
 
 export const Brands = () => (
-  <section className="py-12 sm:py-24 bg-customDarkBg1 w-full mt-16 mb-16">
+  <section className="py-12 sm:py-24 bg-customDarkBg1 w-full mt-64 mb-16">
     <motion.div
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
@@ -20,33 +20,26 @@ export const Brands = () => (
           <div className="w-full lg:w-1/2 px-4 mb-12 lg:mb-0">
             <div className="flex flex-col">
               <h2 className="mb-2  text-4xl sm:text-5xl 2xl:text-6xl font-bold tracking-normal text-white">
-                Trusted by brands
+                Trusted by our
               </h2>
               <h2 className=" text-4xl  sm:text-5xl 2xl:text-6xl font-bold tracking-normal text-customSecondary">
-                all over the world
+                partners and customers
               </h2>
             </div>
           </div>
           <div className="w-2/3 sm:w-[620px] lg:w-1/2 mx-auto lg:mx-0 lg:pl-10">
-            <div className="flex flex-wrap -m-4">
-              <div className="w-1/2 sm:w-1/3 py-6 flex justify-center">
-                <AmazonLogo />
+            <div className="flex justify-center space-x-6 flex-wrap -m-4">
+              <div className="w-64 py-6 flex justify-center">
+                <img src="/capital-factory-logo-light.svg" alt="capital-factory" />
               </div>
-              <div className="w-1/2 sm:w-1/3 py-6 flex  justify-center">
-                <DropboxLogo />
+              <div className="w-64 py-6 flex  justify-center">
+                <img src="/chicago-debate.png" alt="Chicago Debate Society" />
               </div>
-              <div className="w-1/2 sm:w-1/3 py-6 flex  justify-center">
-                <NetflixLogo />
+              <div className="w-64 py-6 flex  justify-center">
+                <img src="/FutoMainLogo.svg" alt="Futo" />
               </div>
-              <div className="w-1/2 sm:w-1/3 py-6 flex justify-center">
-                <StripeLogo />
-              </div>
-
-              <div className="w-1/2 sm:w-1/3 py-6 flex justify-center">
-                <SpotifyLogo />
-              </div>
-              <div className="w-1/2 sm:w-1/3  py-6 flex  justify-center">
-                <SlackLogo />
+              <div className="w-64 py-6 flex justify-center">
+                <img src="/rr-logo-square-silver-gold-large.png" alt="RR" />
               </div>
             </div>
           </div>
